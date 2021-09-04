2dffae463c58dd48c7c9fcff7a35216d6c3342f4
Foxbyte8 <mjxyz8@gmail.com>
2021-09-03 21:15:32 -0500

    added comments to explain the code

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import Phaser from 'phaser';
+import Phaser from 'phaser'; // import phaser
 
+// configure the phaser game
 const config = {
   width: 800,
   height: 600,
-  type: Phaser.AUTO,
+  type: Phaser.AUTO, // default to webgl unless unsupported use canvas
 };
 
+// create a phaser game
 const game = new Phaser.Game(config);
